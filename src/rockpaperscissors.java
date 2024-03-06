50649583df3eb0e07e107f00cc37af8ebc093028
MKEtzler2024 <117937054+MKEtzler2024@users.noreply.github.com>
2024-03-06 13:06:08 -0600

    Update rockpaperscissors.java
    Finished game

--- src/rockpaperscissors.java
+++ src/rockpaperscissors.java
@@ -1,44 +1,76 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class rockpaperscissors{
-public static void main(){
-  Scanner scnr = new Scanner(System.in);
-  String playerMove = "";
-  String aiMove = "";
-  String winner = "";
-  System.out.println("Welcome to Tic Tak Toe");
-  System.out.println("Your moves are: Rock, Paper, Scissors, Spock");
-  System.out.print("Enter your move: ");
-  playerMove = scnr.next().ignoreCase();
-  aiMove = aiMove();
-  winner = checkWinner(playerMove, aiMove);
-}
-  public static String aiMove(){
-    String aiMove = "";
-    randomNum = Math.randomInt(0,5);
-    switch randomNum{
-      case 0:
-        aiMove = "Lizard";
-      case 1:
-        aiMove = "Rock";
-      case 2:
-        aiMove = "Paper";
-      case 3: 
-        aiMove = "Scissors";
-      case 4: 
-        aiMove = "Spock";
+    public static void main(String [] args){
+        Scanner scnr = new Scanner(System.in);
+        String playerMove;
+        String aiMove;
+        boolean winner = false;
+        System.out.println("Welcome to Tic Tak Toe");
+        System.out.println("Your moves are: Rock, Paper, Scissors, Spock");
+        System.out.print("Enter your move: ");
+        playerMove = scnr.next().toLowerCase();
+        aiMove = aiMove();
+        winner = checkWinner(playerMove, aiMove);
+        if (winner == true){
+            System.out.println("You won!");
+        }
+        else{
+            System.out.println("You lost");
+        }
     }
-    return aiMove;
-  }
-  public static String checkWinner(String playerMove, String aiMove){
-    //Rock beats scissor and lizard
-    //Paper beats rock and spock
-    //scissors beats paper and lizard
-    //lizard beats spock and paper
-    //spock beats scissors and rock
-    if((playerMove=="rock")&&((aiMove =="")||(aiMove ==""))){}
-    if((playerMove=="paper")&&((aiMove =="")||(aiMove ==""))){}
-    if((playerMove=="scissors")&&((aiMove =="")||(aiMove ==""))){}
-    if((playerMove=="lizard")&&((aiMove =="")||(aiMove ==""))){}
-    if((playerMove=="spock")&&((aiMove =="")||(aiMove ==""))){}
-  }
+    public static String aiMove(){
+        String aiMove = "";
+        int randomNum = 1 + (int)(Math.random() * 5);
+        System.out.println(randomNum);
+        switch (randomNum){
+            case 1:
+                aiMove = "Lizard";
+                break;
+            case 2:
+                aiMove = "Rock";
+                break;
+            case 3:
+                aiMove = "Paper";
+                break;
+            case 4:
+                aiMove = "Scissors";
+                break;
+            case 5:
+                aiMove = "Spock";
+                break;
+        }
+        System.out.println("Ai plays: " +aiMove);
+        return aiMove.toLowerCase();
+    }
+    public static boolean checkWinner(String playerMove, String aiMove){
+        //Rock beats scissor and lizard
+        //Paper beats rock and spock
+        //scissors beats paper and lizard
+        //lizard beats spock and paper
+        //spock beats scissors and rock
+        boolean winner = false;
+        if (playerMove.equals(aiMove)){
+            System.out.println("You tied");
+        }
+        else if((playerMove.equals("rock")&&((aiMove.equals("scissors"))||(aiMove.equals("lizard"))))){
+            winner =true;
+        }
+        else if((playerMove.equals("paper"))&&((aiMove.equals("rock"))||(aiMove.equals("spock")))){
+            winner =true;
+        }
+        else if((playerMove.equals("scissors"))&&((aiMove.equals("paper"))||(aiMove.equals("lizard")))){
+            winner =true;
+        }
+        else if((playerMove.equals("lizard"))&&((aiMove.equals("spock"))||(aiMove.equals("paper")))){
+            winner =true;
+        }
+        else if((playerMove.equals("spock"))&&((aiMove.equals("scissors"))||(aiMove.equals("rock")))){
+            winner =true;
+        }
+        else {
+            winner = false;
+        }
+        return winner;
+    }
 }
